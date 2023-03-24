e268748384ffeb9e3fdb7677889928c104b33abf
enri-ge <enrico.alletto@gmail.com>
2023-03-24 21:33:14 +0100

    Favorite applications OK

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7,18 +7,33 @@
 # Script Description:   This script configures some aesthetic GNOME parameters
 
 # *** favorite-apps ***
-gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'firefox_firefox.desktop', 'org.gnome.Nautilus.desktop', 'snap-store_ubuntu-software.desktop', 'yelp.desktop', 'org.gnome.Terminal.desktop']"
+
+filename="favourites.txt"
+
 echo ""
 echo "-------------------------------------------------"
-echo "Firefox"
-echo "Nautilus"
-echo "Ubuntu Software"
-echo "Help"
-echo "Terminal"
+
+while read line; do
+# read each line
+testoBis="$line, $testoBis"
+echo $line
+done < $filename
+
 echo ""
 echo "DONE - Favorite applications correctly configured"
 echo "-------------------------------------------------"
 echo ""
+#testoBis1=$($testoBis | awk '{print substr($0,1,length($0)-1)}')
+testoBis1=$(echo $testoBis | awk '{print substr($0,1,length($0)-1)}')
+#echo $testoBis1
+
+#gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'firefox_firefox.desktop', 'org.gnome.Nautilus.desktop', 'snap-store_ubuntu-software.desktop', 'yelp.desktop', 'org.gnome.Terminal.desktop']"
+gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), $testoBis1]"
+
+#******************************************
+# dconf read /org/gnome/shell/favorite-apps
+#******************************************
+
 # *** favorite-apps ***
 
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top false
